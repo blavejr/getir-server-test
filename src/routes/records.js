e01dcf85576f307eef81b1fe55e2e43c08a58241
remigius.kalimba <remigius.kalimba@flexperto.com>
2022-02-20 08:54:13 +0100

    add basic error handling

--- src/routes/records.js
+++ src/routes/records.js
@@ -6,16 +6,26 @@ const helpers = require('../helpers/index');
 router.get('/', (req, res) => {
     res.send('I work bro!')
 }).post('/', async (req, res) => {
+    try {
     const {startDate, endDate, minCount, maxCount} = req.query;
-    const data = await recordsController.getAllRecords()
+    const data = await recordsController.getAllRecords();
     const filterdByDate = helpers.getRecordsBetweanDate(data, startDate, endDate)
     const filterdByCount = helpers.getRecordsBetweanCount(filterdByDate, minCount, maxCount)
     res.status(200).json({
         code: 0,
-        msg: 'askdbkajsdb',
+        msg: 'success',
         records: filterdByCount,
         count: filterdByCount.length
     })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            code: 1,
+            msg: 'Internal server error',
+            records: [],
+            count: 0
+        })
+    }
 })
 
 module.exports = router;
